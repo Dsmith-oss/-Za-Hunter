783a41d997c1f4178d38bcf70890be6f4bdf2dbd
Declan Smith <dmansmith03@yahoo.com>
2021-03-09 10:22:37 -0600

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Declan Smith on 3/9/21.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
