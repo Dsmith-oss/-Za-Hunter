e1771d6d9b26fd7c75153bda31bc2b47d9c5725d
Declan Smith <dmansmith03@yahoo.com>
2021-03-10 10:30:09 -0600

    Added pizza icon and link to store website

---
+++ 'Za Hunter/LocationManager.swift
@@ -0,0 +1,21 @@
+//
+//  LocationManager.swift
+//  'Za Hunter
+//
+//  Created by Declan Smith on 3/9/21.
+//
+
+import Foundation
+import CoreLocation
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    
+    override init() {
+           super.init()
+           locationManager.delegate = self
+           locationManager.requestWhenInUseAuthorization()
+           locationManager.startUpdatingLocation()
+
+       }
+}
+
